374904da13cce1568bd058b621a5107add60ea4f
rinalds98 <rinalds98@gmail.com>
2022-11-04 11:15:07 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -2,8 +2,10 @@
 Tool for sales market
 """
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -64,6 +66,25 @@ def update_sales_worksheet(data):
     print("sales worksheet updated succesfully\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    compare sales data
+    """
+    print("calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to love sandwiches Data Automation\n")
+main()
